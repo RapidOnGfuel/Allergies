d3eb6fcd4c62a797a5d6d9ca8ea41087714b297b
slasherthunder <138834250+slasherthunder@users.noreply.github.com>
2024-09-25 18:52:26 -0700

    made some changes to the tracing

--- nut_enemy.gd
+++ nut_enemy.gd
@@ -1,24 +1,38 @@
 extends CharacterBody2D
 
-var speed = 50
+var speed = 250  
 var player_chase = false
-var player = null  
+var player = null
+var idle_timer = 0  
+var chasing_after_idle = false  
 
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position).normalized() * speed * delta
 		$AnimatedSprite2D.play("walking")
 		
-		if(player.position.x - position.x) < 0:
-			$AnimatedSprite2D.flip_h = false
-		else: 
-			$AnimatedSprite2D.flip_h = true
+		
+		$AnimatedSprite2D.flip_h = player.position.x > position.x
 	else:
-		$AnimatedSprite2D.play("idle")
+		if chasing_after_idle:
+			idle_timer -= delta
+			print(int(idle_timer))  
+
+			if idle_timer <= 0:
+				chasing_after_idle = false 
+				player_chase = true  
+				print("Chasing!")
+		else:
+			$AnimatedSprite2D.play("idle")
+
 func _on_detection_area_body_entered(body):
 	player = body
-	player_chase = true
+	player_chase = false  
+	idle_timer = 3  
+	chasing_after_idle = true  
+	print(int(idle_timer))  
 
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+	chasing_after_idle = false  
