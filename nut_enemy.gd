4132642367c0df74f4b7da6bf9698cc843224998
Jer0st <164413660+Jer0st@users.noreply.github.com>
2024-09-25 18:58:22 -0700

    started inventory system

--- nut_enemy.gd
+++ nut_enemy.gd
@@ -1,38 +1,24 @@
 extends CharacterBody2D
 
-var speed = 250  
+var speed = 50
 var player_chase = false
-var player = null
-var idle_timer = 0  
-var chasing_after_idle = false  
+var player = null  
 
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position).normalized() * speed * delta
 		$AnimatedSprite2D.play("walking")
 		
-		
-		$AnimatedSprite2D.flip_h = player.position.x > position.x
+		if(player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = false
+		else: 
+			$AnimatedSprite2D.flip_h = true
 	else:
-		if chasing_after_idle:
-			idle_timer -= delta
-			print(int(idle_timer))  
-
-			if idle_timer <= 0:
-				chasing_after_idle = false 
-				player_chase = true  
-				print("Chasing!")
-		else:
-			$AnimatedSprite2D.play("idle")
-
+		$AnimatedSprite2D.play("idle")
 func _on_detection_area_body_entered(body):
 	player = body
-	player_chase = false  
-	idle_timer = 3  
-	chasing_after_idle = true  
-	print(int(idle_timer))  
+	player_chase = true
 
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
-	chasing_after_idle = false  
