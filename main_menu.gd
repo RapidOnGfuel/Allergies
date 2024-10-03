9d2428404baf7c181c37297b594736eb18603d9c
slasherthunder <138834250+slasherthunder@users.noreply.github.com>
2024-10-03 09:57:58 -0700

    finished main menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,11 +1,14 @@
+class_name Main_Menu
 extends Control
 
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var start_level = preload("res://Level_1.tscn") as PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+func on_exit_pressed() -> void:
+	get_tree().quit()
